711bc2d1b2cd9368ba481e53a78144bad936ced4
Demitrus Booker <demitrusw.b@gmail.com>
2024-04-15 20:21:09 -0700

    setting up command line and user interface arguments

--- otp_python.py
+++ otp_python.py
@@ -1,60 +1,54 @@
 import argparse
 
-# parser = argparse.ArgumentParser(description = 'Process some integers.')
-# parser.add_argument('integers', metavar = 'N', type = int, nargs = '?', help = 'an integer for the accumulator')
-# parser.add_argument('--sum', dest = 'accumulate', const = 'sum', default = max, 
-#                     help = 'sum the integers (default is finding the max value)')
-# args = parser.parse_args()
-# print(args.accumulate(args.integers))
-
-# parser = argparse.ArgumentParser(
-#     prog = 'Process some integers',
-#     description="will process and return result of two integers",
-#     epilog = 'blah')
-
-# parser.add_argument('integers', metavar = 'N', type = int, nargs = '+',
-#                     help = 'an integer for the accumulator')
-# parser.add_argument('--sum', dest='accumulate', action='store_const',
-#                     const=sum, default=max,
-#                     help='sum the integers (default: find the max)')
-
-# args = parser.parse_args()
-# print(args.filename, args.count, args.verbose)
-
-# parser = argparse.ArgumentParser()
-# parser = argparse.ArgumentParser(prog='myprogram')
-# parser.add_argument('--foo', help='foo help')
-# args = parser.parse_args()
-
-# parser = argparse.ArgumentParser()
-# parser.add_argument('filename')
-# args = parser.parse_args()
-# print(args)
-
-# Spec out for command line
-    # - will I be able to write everything on the commandline as args and use throughout program???
-    # generating key file
-        # - maybe I don't need the key file here --> just need the count of chars to be generated???
-        # C: [binary file] [-a] [key] [key count] [file name]
-        # P: [python3] []
-    # encodeing
-        # here will need plaintext, will need key file
-     #decoding
-        # will need encoded file and key file
-
+def encode(key):
+    return "a"
 
 def main():
+    #create and fill parser with appropriate arguments
     parser = argparse.ArgumentParser()
     #options for command line
     # add key generation information
-    parser.add_argument('-k', '--key', dest = 'key file', action = 'store_const', metavar = 'N', type = int, const = 256, help = 'Count of chars to generate for key file to hold')
-    parser.add_argument('-e', '--encode', dest = 'encode file', help = 'Name of file to hold encoded text (plaintext text converted using key)')
-    parser.add_argument('-d', '--decode', dest = 'decode file', help='Name of file holding decoded messaage (encoded text converted to plaintext)')
-    
-
+    parser.add_argument('-k', '--key', dest = 'key_file', default= 256, help = 'count of chars to generate for key file to hold')
+    parser.add_argument('-e', '--encode', dest = 'encode_file', help = 'name of file to hold encoded text (plaintext text converted using key)')
+    parser.add_argument('-d', '--decode', dest = 'decode_file', help='name of file holding decoded messaage (encoded text converted to plaintext)')
+    parser.add_argument('-p', '--plaintext', help = 'the message to be encoded') 
     args = parser.parse_args()
+
+    if args.key_file is not None:
+        pass
+        # open file key.txt file for writing
+        f1 = open("key.txt", 'w+')
+        alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
+        # generate random number to use to capture random char from alpha var
+        #pull value from alpha --> rand_char = alpha[value obtained from random numb generator]
+        #write to opened file
+        f1.write(alpha)
+        #add newline char
+        f1.close()
+    else:
+        file1 = open("key.txt", "r")
+            #add exception
+        if args.encode_file is not None:
+            ###plaintext redirected from cat plaintext.txt###
+            #read in key file
+            key = f1.read()
+                #add exception
+            #get len of key file
+            #generate ciphertext by calling fx passing key, plaintext
+            ciphertext = encode(key)
+            print(ciphertext)
+
+        if args.decode_file is not None:
+            pass
+        #open key file
+                #add exception
+            ###ciphertext coming from stdout/redirection###
+            #generate new_plaintext by passing key and ciphertext to appropriate fx
+        file1.close()
     print(args)
-    # print(args.key)
+    print(args.plaintext)
+    
 
 if __name__ == "__main__":
     main()
+
